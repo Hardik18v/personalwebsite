290c6e36ae1850bc71c5c9395cbf141ac36358b1
Hardik18v <86034284+Hardik18v@users.noreply.github.com>
2021-12-17 16:40:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let btn = document.querySelector("#btn");
+let displaydiv = document.querySelector(".toggle");
+
+btn.addEventListener("click", () => {
+  if (displaydiv.style.display != "flex") {
+    displaydiv.style.animation = "show .5s  forwards";
+    displaydiv.style.display = "flex";
+    btn.innerHTML = `<img src="./Img/upload.png" alt="">`;
+  } else {
+    displaydiv.style.display = "none";
+    btn.innerHTML = `<img src="./Img/down-arrow.png" alt="">`;
+  }
+});
+
+
 console.log('Its working')
 
 let theme = localStorage.getItem('theme')
